41c643b8dc20aae722db285aa9f3cc816296d09b
Rosa <costurezati@gmail.com>
2025-05-22 19:33:47 -0300

    .

---
+++ api/logic.php
@@ -0,0 +1,5 @@
+<?php
+include ("/laragon/www/conexaolocal/api/config.php");
+
+
+?>
--- app/index.php
+++ app/index.php
@@ -1,9 +1,18 @@
+<?php
+include ("/laragon/www/conexaolocal/api/config.php");
+include ("/laragon/www/conexaolocal/api/logic.php");
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conexão Local</title>
+    <link rel="stylesheet" href="./ressources/css/style.css">
 </head>
 <body>
     
