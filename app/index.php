4bac11125d512ad1af5a39d510185ff5717ca4d6
Rosa <costurezati@gmail.com>
2025-05-24 18:09:43 -0300

    .

--- app/index.php
+++ app/index.php
@@ -17,6 +17,7 @@ include("/laragon/www/conexaolocal/api/logic.php");
     <link rel="stylesheet" href="./ressources/css/home.css">
     <link rel="stylesheet" href="./ressources/css/evento_org.css">
     <link rel="stylesheet" href="./ressources/css/eventointercoment.css">
+    <link rel="stylesheet" href="./ressources/css/media.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
